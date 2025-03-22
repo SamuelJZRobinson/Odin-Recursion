6779bb1b0895bacc397c7ea5aa6e1fa5f3c5f9b2
Samuel Robinson <samueljzrobinson@gmail.com>
2025-03-22 18:04:34 +1000

    Fibonacci Recursive

--- main.js
+++ main.js
@@ -6,4 +6,12 @@ function fibsIte(digitCount){
   return fibList;
 }
 
-console.log(fibsIte(8));
+// console.log(fibsIte(8));
+
+function fibsRec(digitCount){
+  let fibList = [0,1];
+  if (digitCount <= 1) return fibList;
+  fibList.push(fibList[digitCount-1] + fibList[digitCount-2]);
+}
+
+// console.log(fibsIte(8));
